2a685a70bd08d00332a4dc5abf95227400d4e79b
PrestonGarcia <prestongarcia77@gmail.com>
2019-05-17 22:34:04 -0700

    Added in AI to go for middle, look for win conditions, and go for corners if middle is taken

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,11 +5,19 @@ from win32api import GetSystemMetrics
 
 master = tk.Tk()
 master.title("Tic Tac Toe")
-master.geometry("{}x{}".format(int(GetSystemMetrics(0) / 3), int(GetSystemMetrics(1) / 2)))
+master.geometry("{}x{}".format(int(GetSystemMetrics(0) / 3 - GetSystemMetrics(0) / 9), int(GetSystemMetrics(1) / 2 - GetSystemMetrics(1) / 20)))
 turn = 'X'
 win = False
 turnCount = 1
-
+def checkForMatches(buttonOne, buttonTwo, buttonThree):
+    if (buttonOne == 'O' or buttonOne == 'X') and (buttonTwo == 'O' or buttonTwo == 'X') and (buttonThree == 'O' or buttonThree == 'X'):
+        return False
+    elif buttonOne == buttonTwo != buttonThree:
+        return buttonThree
+    elif buttonOne == buttonThree != buttonTwo:
+        return buttonTwo
+    elif buttonTwo == buttonThree != buttonOne:
+        return buttonOne
 def Invalid():
     messagebox.showerror(title="Invalid", message= "Invalid move!")
 def turnChange():
@@ -30,27 +38,70 @@ def turnChange():
         winMessage = turn + " has won the game!"
         if (buttonA1 == turn and buttonB1 == turn and buttonC1 == turn):
             messagebox.showinfo(title="%s won!" % turn, message=winMessage)
+            master.destroy()
         elif (buttonA2 == turn and buttonB2 == turn and buttonC2 == turn):
             messagebox.showinfo(title="%s won!" % turn, message=winMessage)
+            master.destroy()
         elif (buttonA3 == turn and buttonB3 == turn and buttonC3 == turn):
             messagebox.showinfo(title="%s won!" % turn, message=winMessage)
+            master.destroy()
         elif (buttonA1 == turn and buttonA2 == turn and buttonA3 == turn):
             messagebox.showinfo(title="%s won!" % turn, message=winMessage)
+            master.destroy()
         elif (buttonB1 == turn and buttonB2 == turn and buttonB3 == turn):
             messagebox.showinfo(title="%s won!" % turn, message=winMessage)
+            master.destroy()
         elif (buttonC1 == turn and buttonC2 == turn and buttonC3 == turn):
             messagebox.showinfo(title="%s won!" % turn, message=winMessage)
+            master.destroy()
         elif (buttonA3 == turn and buttonB2 == turn and buttonC1 == turn):
             messagebox.showinfo(title="%s won!" % turn, message=winMessage)
+            master.destroy()
         elif (buttonA1 == turn and buttonB2 == turn and buttonC3 == turn):
             messagebox.showinfo(title="%s won!" % turn, message=winMessage)
+            master.destroy()
         elif turnCount == 10:
             messagebox.showerror(title="Tie", message="It's a Tie!")
+            master.destroy()
         else:
             if turn == 'X':
                 turn = 'O'
                 messagebox.showinfo(title="O's turn", message="O's turn")
-                buttonPressed = random.choice(buttonList)
+                buttonPressed = False
+                while not buttonPressed:
+                    buttonPressed = checkForMatches(buttonA1, buttonB1, buttonC1)
+                    if buttonPressed:
+                        break
+                    buttonPressed = checkForMatches(buttonA2, buttonB2, buttonC2)
+                    if buttonPressed:
+                        break
+                    buttonPressed = checkForMatches(buttonA3, buttonB3, buttonC3)
+                    if buttonPressed:
+                        break
+                    buttonPressed = checkForMatches(buttonA1, buttonA2, buttonA3)
+                    if buttonPressed:
+                        break
+                    buttonPressed = checkForMatches(buttonB1, buttonB2, buttonB3)
+                    if buttonPressed:
+                        break
+                    buttonPressed = checkForMatches(buttonC1, buttonC2, buttonC3)
+                    if buttonPressed:
+                        break
+                    buttonPressed = checkForMatches(buttonA1, buttonB2, buttonC3)
+                    if buttonPressed:
+                        break
+                    buttonPressed = checkForMatches(buttonC1, buttonB2, buttonA3)
+                    break
+
+                if not buttonPressed:
+                    if buttonB2 in buttonList:
+                        buttonPressed = buttonB2
+                    elif len(cornerButtons) != 0:
+                        buttonPressed = random.choice(cornerButtons)
+                    else:
+                        buttonPressed = random.choice(buttonList)
+
+
                 buttonPressed.invoke()
 
             elif turn == 'O':
@@ -61,6 +112,7 @@ def turnChange():
 def A1():
     global buttonA1
     buttonList.remove(buttonA1)
+    cornerButtons.remove(buttonA1)
     buttonA1 = turn
     button = tk.Button(master, text=turn, width= int(GetSystemMetrics(0) / 100), height=int(GetSystemMetrics(1) / 100)
                        , command=Invalid)
@@ -77,6 +129,7 @@ def A2():
 def A3():
     global buttonA3
     buttonList.remove(buttonA3)
+    cornerButtons.remove(buttonA3)
     buttonA3 = turn
     button = tk.Button(master, text=turn, width= int(GetSystemMetrics(0) / 100), height=int(GetSystemMetrics(1) / 100)
                        , command=Invalid)
@@ -109,6 +162,7 @@ def B3():
 def C1():
     global buttonC1
     buttonList.remove(buttonC1)
+    cornerButtons.remove(buttonC1)
     buttonC1 = turn
     button = tk.Button(master, text=turn, width= int(GetSystemMetrics(0) / 100), height=int(GetSystemMetrics(1) / 100)
                        , command=Invalid)
@@ -126,6 +180,7 @@ def C2():
 def C3():
     global buttonC3
     buttonList.remove(buttonC3)
+    cornerButtons.remove(buttonC3)
     buttonC3 = turn
     button = tk.Button(master, text=turn, width= int(GetSystemMetrics(0) / 100), height=int(GetSystemMetrics(1) / 100)
                        , command=Invalid)
@@ -161,5 +216,6 @@ buttonC3 = tk.Button(master, text="", width= int(GetSystemMetrics(0) / 100), hei
                      , command=C3)
 buttonC3.grid(row=3, column=3)
 buttonList = [buttonA1, buttonA2, buttonA3, buttonB1, buttonB2, buttonB3, buttonC1, buttonC2, buttonC3]
+cornerButtons = [buttonA1, buttonC1, buttonA3, buttonC3]
 
 tk.mainloop()
